2f4ce526a0d7c48455a76cc805970c92b401a852
Jason Tiernan <jason.tiernan@me.com>
2015-07-07 23:32:47 -0400

    Updated Design
    Updated iconography, styles, etc.

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,29 @@
 $(document).ready(function() {
 	
-
 	$('.start-button').click(function() {
 		intro();
 	});
 
 	function intro() {
 		$('.intro').css('opacity','0');
-		$('.shame-button').addClass('show-button');
-		$('.start-button').css('display','none');
+		$('.shakey').css('opacity','1');
+		$('.start-button').addClass('shame-transform');
 		$('.audioDemo').trigger('play').trigger('pause');
+		$('.start-button').click(function() {
+			shame();
+		});
 	}
 
 
 	$(this).gShake(function() {
 		shame();
 	});
-	
-	$('.shame-button').click(function() {
-		shame();
-	});
 
 	function shame() {
-		console.log("Shame");
-		$('.shame-button').addClass('ring');
+		$('.shakey').addClass('ring');
 		$('.audioDemo').trigger('play');
 		setTimeout(function() {
-			$('.shame-button').removeClass('ring');
+			$('.shakey').removeClass('ring');
 		}, 2000);
 	}
 });
